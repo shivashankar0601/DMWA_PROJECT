9d7c911890d5981e6be30d42c2e53d6d0dd7c754
Hardee Garala <hardeegarala@Hardees-MacBook-Air.local>
2022-04-09 00:49:33 -0300

    add create table processing

--- project/src/main/java/com/example/project/QueryManager/TableProcessor.java
+++ project/src/main/java/com/example/project/QueryManager/TableProcessor.java
@@ -20,10 +20,14 @@ public class TableProcessor {
         // take this method as starting point and start processing the query, after the DistributedDatabaseLayer is completed, we will have the api to be hit
         query = query.toLowerCase();
         String insertIntoRegx = "(insert\\sinto\\s[)(0-9a-zA-Z_\\s,'\"]+)[;]?"; //check if the insert query is in correct format
-
+        String createTableRegx = "^(create\\stable\\s[)(0-9a-zA-Z_\\s,]+)[;]?$";
         if (query.matches(insertIntoRegx)) {
             insertIntoQuery(query,"local");
-        } else {
+        }
+        else if (query.matches(createTableRegx)) {
+            createTableQuery(query);
+        }
+        else {
             System.out.println("Query is not correct");
         }
     }
@@ -117,5 +121,70 @@ public class TableProcessor {
             e.printStackTrace();
         }
         return false;
+    }
+
+    private static void createTableQuery(String query){
+        String tablePath;
+        String tableName;
+        String output = "";
+        String columnsAndValues;
+
+        try {
+            //please remove currentDbName variable added just for testing
+            tablePath = Utils.currentDbName+ "/"+(query.split("\s")[2]) + ".tsv";
+            Pattern p = Pattern.compile("(.+?)\\s+(.+?)\\s+(.+?)\\s+(.*)");
+            Matcher m = p.matcher(query);
+            if (m.find()) {
+                tableName = m.group(3);
+                if(!checkIfTableExists(tableName)) {
+
+                    if (m.group(4).contains(";")) {
+                        if (m.group(4).startsWith("(")) {
+                            columnsAndValues = m.group(4).substring(1, m.group(4).indexOf(';') - 1);
+                        } else {
+                            columnsAndValues = m.group(4).substring(0, m.group(4).indexOf(';'));
+                        }
+                    }
+                    else {
+                        if (m.group(4).startsWith("(")) {
+                            columnsAndValues = m.group(4).substring(1, m.group(4).length() - 1);
+                        } else {
+                            columnsAndValues = m.group(4).substring(0, m.group(4).length());
+                        }
+                    }
+                    String[] columnInfo = columnsAndValues.split(",");
+                    int columnCount = columnInfo.length;
+                    int notColumnCount = 0;
+                    for (String column : columnInfo) {
+                        if (column.toLowerCase().contains("foreign key") || column.toLowerCase().contains("primary key")) {
+                            notColumnCount++;
+                        }
+                        output += column.replaceAll("[()]", " ").replaceAll("  ", " ").trim() + "~";
+                    }
+                    columnCount = columnCount - notColumnCount;
+                    output = "columnCount~" + columnCount + "\n" + output;
+
+                    output = output.substring(0, output.length() - 1);
+                    FileWriter fileWriter = new FileWriter(Utils.resourcePath+tablePath, true);
+                    fileWriter.append(output);
+                    fileWriter.close();
+                    FileWriter fw = new FileWriter(Utils.resourcePath+Utils.currentDbName+"/metadata.tsv", true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter out = new PrintWriter(bw);
+                    out.print(tableName+"~");
+                    out.close();
+                    bw.close();
+                    fw.close();
+                    System.out.println("Table created");
+
+                }
+                else{
+                    System.out.println("Table already exists!");
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
